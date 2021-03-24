fb143532143befed9d4b76987d136586c8b069ae
Lawrence Whalen <ldwhalen@gmail.com>
2021-03-24 15:35:17 -0600

    Scroll up to view the monstrosity you created trying to undo your early pull request and scream into the night then fix and refactor

--- lib/venue.rb
+++ lib/venue.rb
@@ -14,57 +14,14 @@ class Venue
   end
 
   def yell_at_patrons
-    @patrons_upcase = []
-    @patrons.each do |patron|
-      @patrons_upcase.push(patron.upcase!)
-    end
-  end
-
-  def over_capacity
-    @patrons.count > @capacity
-  end
-
-  def kick_out
-    while @patrons.count > @capacity
-      @patrons.shift
-    end
-  end
-end
-class Venue
-end
-class Venue
-  attr_reader :name,
-              :capacity,
-              :patrons
-
-  def initialize(name, capacity)
-    @name = name
-    @capacity = capacity
-    @patrons = []
-  end
-
-  def add_patron(patron_name)
-    @patrons.push(patron_name)
-  end
-
-  def yell_at_patrons
     @patrons.map { |patron| patron.upcase!  }
   end
 
-  # def yell_at_patrons
-  #   @patrons_upcase = []
-  #   @patrons.each do |patron|
-  #     @patrons_upcase.push(patron.upcase!)
-  #   end
-  # end
-
   def over_capacity
     @patrons.count > @capacity
   end
 
   def kick_out
-    while @patrons.count > @capacity
-      @patrons.shift
-    end
+    @patrons.shift(@patrons.count - @capacity)
   end
 end
